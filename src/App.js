82a7038354ec486ef2be784a454336855b1bc10b
Garry Brother <garry.m.brother@gmail.com>
2023-03-19 10:23:17 -0400

    Richly featured custom Button component with CSS (tailwind) styling

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+
+import { GoBell } from 'react-icons/go';
+import Button from "./Button";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div>
+            <div>
+                <Button success rounded outline className={'mb-5'}>
+                    <GoBell/>
+                    Click me!!
+                </Button>
+            </div>
+            <div>
+                <Button danger outline>
+                    <GoBell/>
+                    Buy Now!
+                </Button>
+            </div>
+            <div>
+                <Button warning>
+                    <GoBell/>
+                    See deal!
+                </Button>
+            </div>
+            <div>
+                <Button secondary outline>
+                    <GoBell/>
+                    Hide Ads!
+                </Button>
+            </div>
+            <div>
+                <Button primary rounded>
+                    <GoBell/>
+                    Something!
+                </Button>
+            </div>
+        </div>
+    );
 }
 
 export default App;
